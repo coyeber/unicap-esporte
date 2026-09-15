ac7a10dae7a2e6cbc3b9abe35076aa0bf069924f
Sergio <sergiobjunior232@gmail.com>
2026-09-15 16:28:51 -0300

    Atualiza URL do Apps Script

--- config.js
+++ config.js
@@ -1,4 +1,2 @@
-/**
- * UNICAP Esporte — backend Google Apps Script
- */
-window.UNICAP_API_URL = "https://script.google.com/macros/s/AKfycbycGkh5-ihx0JeY_6ySBLTMGpM43JsCE8JQ5xB8aHNhnQpUJ32k8WBicyQmwzqwUJyO/exec";
+window.UNICAP_API_URL =
+  "https://script.google.com/macros/s/AKfycbzBNAFmmtqgMq89aBKSH1GZus_b46-jUlGKUh4cjLaqzO1TfM_QFuh8vltQndZ2LYSi/exec";

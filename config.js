1fe086b54c8a46e90e4c0e0c1fd7902c7d552f07
Sergio <sergiobjunior232@gmail.com>
2026-09-15 16:37:44 -0300

    Atualiza URL ipt

--- config.js
+++ config.js
@@ -1,2 +1,4 @@
-window.UNICAP_API_URL =
-  "https://script.google.com/macros/s/AKfycbzBNAFmmtqgMq89aBKSH1GZus_b46-jUlGKUh4cjLaqzO1TfM_QFuh8vltQndZ2LYSi/exec";
+/**
+ * UNICAP Esporte — backend Google Apps Script
+ */
+window.UNICAP_API_URL = "https://script.google.com/macros/s/AKfycbzBNAFmmtqgMq89aBKSH1GZus_b46-jUlGKUh4cjLaqzO1TfM_QFuh8vltQndZ2LYSi/exec";
